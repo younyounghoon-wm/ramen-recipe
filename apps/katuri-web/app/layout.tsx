9fc283b65ad26395731c14bbda7560b7c7e22a15
younyounghoon-wm <yhyoon@wemade.com>
2024-05-02 13:42:40 +0900

    icon

--- apps/katuri-web/app/layout.tsx
+++ apps/katuri-web/app/layout.tsx
@@ -5,6 +5,9 @@ export const metadata: Metadata = {
   title: "까투리 할일로그",
   description: "for katuri",
   manifest: "/manifest.json",
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [{ rel: "icon", url: "/assets/logo.png", sizes: "192x192" }],
 };
 
 export default function RootLayout({
