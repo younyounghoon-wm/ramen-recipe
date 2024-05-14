13fb134e76e94e58fcee8519d4df841fecc2e6fd
younyounghoon-wm <yhyoon@wemade.com>
2024-05-14 15:50:41 +0900

    [수정] 푸시 테스트

--- apps/katuri-web/src/entities/todo/ui/Header/index.tsx
+++ apps/katuri-web/src/entities/todo/ui/Header/index.tsx
@@ -4,8 +4,13 @@ import { dateStyle, description, headerStyle } from "./index.css";
 function Header() {
   const { year, month, date, day } = useTodayDate();
 
+  const handleGo = () => {
+    location.href = "https://elfoxero.github.io/html5notifications/";
+  };
+
   return (
     <header className={headerStyle}>
+      <button onClick={handleGo}>go push site</button>
       <h1 className={dateStyle}>
         {year}.{month}.{date} ({day})
       </h1>
